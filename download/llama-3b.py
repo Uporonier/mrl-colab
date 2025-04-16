df228936aed3fb5176c55ab37807226aaad5f656
Acaller <12556752+acaller@user.noreply.gitee.com>
2025-04-16 10:29:37 +0800

    change path

--- download/llama-3b.py
+++ download/llama-3b.py
@@ -1,2 +1,2 @@
 import os
-os.system('huggingface-cli download --resume-download meta-llama/Llama-3.2-3B --local-dir /content/EmbeddingMerage/models/llama-1b')
+os.system('huggingface-cli download --resume-download meta-llama/Llama-3.2-3B --local-dir /content/mrl-colab/models/llama-3b')
